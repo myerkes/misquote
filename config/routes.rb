8668fb15b05fbfb0a71c91a600aadd373cbc3bbc
myerkes <mattyerkes23@gmail.com>
2021-05-19 10:06:33 -0400

    Change root to Mixed Messages project

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'pages#index'
+  root 'messages#index'
 
   get '/messages' => 'messages#index', as: :messages
   get '/pages' => 'pages#index', as: :pages
